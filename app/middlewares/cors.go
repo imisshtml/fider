61d6e5bc4e2fc9dba30f4ea2a1706cbd9fab8657
sunnova-claudewild <119956140+sunnova-claudewild@users.noreply.github.com>
2024-04-27 23:49:29 -0400

    router setup

--- app/middlewares/cors.go
+++ app/middlewares/cors.go
@@ -1,26 +1,16 @@
 package middlewares
 
 import (
-    "net/http"
-    "github.com/getfider/fider/app/pkg/web"
+	"github.com/getfider/fider/app/pkg/web"
 )
 
 // CORS adds Cross-Origin Resource Sharing response headers
 func CORS() web.MiddlewareFunc {
-    return func(next web.HandlerFunc) web.HandlerFunc {
-        return func(c *web.Context) error {
-            c.Response.Header().Set("Access-Control-Allow-Origin", "*")  // Consider specifying domains in production
-            c.Response.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-            c.Response.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-            
-            // Handle preflight requests
-            if c.Request.Method == "OPTIONS" {
-                c.Response.WriteHeader(http.StatusOK)
-                return nil
-            }
-            
-            return next(c)
-        }
-    }
+	return func(next web.HandlerFunc) web.HandlerFunc {
+		return func(c *web.Context) error {
+			c.Response.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Response.Header().Set("Access-Control-Allow-Methods", "GET")
+			return next(c)
+		}
+	}
 }
-
